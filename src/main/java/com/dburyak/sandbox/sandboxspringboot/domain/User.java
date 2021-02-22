adc935182be1f6bab0aacce33172fb8282f3cf82
Dmytro Buryak <dmytro.buryak@gmail.com>
2021-02-23 00:55:29 +0200

    Configured logger

--- src/main/java/com/dburyak/sandbox/sandboxspringboot/domain/User.java
+++ src/main/java/com/dburyak/sandbox/sandboxspringboot/domain/User.java
@@ -2,19 +2,22 @@ package com.dburyak.sandbox.sandboxspringboot.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-@NoArgsConstructor
 @RequiredArgsConstructor(onConstructor_ = {@PersistenceConstructor})
 @SuperBuilder(toBuilder = true)
 @Document(collation = "users")
 public class User extends PersistentVersionedEntity {
+    private final String firstName;
+    private final String lastName;
+    private final LocalDate birthDate;
 }
