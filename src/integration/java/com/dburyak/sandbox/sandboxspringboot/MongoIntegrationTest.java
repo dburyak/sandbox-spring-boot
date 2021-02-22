c3ff251d740fc630a312f21007dfe332fd429981
Dmytro Buryak <dmytro.buryak@gmail.com>
2021-02-22 23:59:32 +0200

    Configured logger and tests

---
+++ src/integration/java/com/dburyak/sandbox/sandboxspringboot/MongoIntegrationTest.java
@@ -0,0 +1,7 @@
+package com.dburyak.sandbox.sandboxspringboot;
+
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+@DataMongoTest
+public class MongoIntegrationTest {
+}
---
+++ src/integration/java/com/dburyak/sandbox/sandboxspringboot/repository/UserRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.dburyak.sandbox.sandboxspringboot.repository;
+
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+@DataMongoTest
+public class UserRepositoryTest {
+
+
+}
