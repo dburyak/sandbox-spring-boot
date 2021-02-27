18162f152467b307170fa9c4e589d2f804aac00e
Dmytro Buryak <dmytro.buryak@gmail.com>
2021-02-27 13:42:39 +0200

    Fine-tuned log4j config

--- src/integration/java/com/dburyak/sandbox/sandboxspringboot/MongoIntegrationTest.java
+++ src/integration/java/com/dburyak/sandbox/sandboxspringboot/MongoIntegrationTest.java
@@ -1,7 +1,43 @@
 package com.dburyak.sandbox.sandboxspringboot;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import reactor.test.StepVerifier;
 
 @DataMongoTest
+@ActiveProfiles("integration-test")
+@Testcontainers
 public class MongoIntegrationTest {
+    public static final String MONGO_VERSION = "4.4.4";
+
+    @Autowired
+    private ReactiveMongoOperations mongo;
+
+    @Container
+    public static final MongoDBContainer MONGO_CONTAINER = new MongoDBContainer(
+            DockerImageName.parse("mongo:" + MONGO_VERSION));
+
+    @DynamicPropertySource
+    public static void mongoProperties(DynamicPropertyRegistry reg) {
+        reg.add("spring.data.mongodb.host", MONGO_CONTAINER::getHost);
+        reg.add("spring.data.mongodb.port", MONGO_CONTAINER::getFirstMappedPort);
+    }
+
+    @BeforeEach
+    public void initDb() {
+        StepVerifier.create(mongo.getCollectionNames()
+                .flatMap(col -> mongo.dropCollection(col)))
+                .verifyComplete();
+        StepVerifier.create(mongo.getCollectionNames())
+                .verifyComplete();
+    }
 }
--- src/integration/java/com/dburyak/sandbox/sandboxspringboot/repository/UserRepositoryTest.java
+++ src/integration/java/com/dburyak/sandbox/sandboxspringboot/repository/UserRepositoryTest.java
@@ -1,9 +1,38 @@
 package com.dburyak.sandbox.sandboxspringboot.repository;
 
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import com.dburyak.sandbox.sandboxspringboot.MongoIntegrationTest;
+import com.dburyak.sandbox.sandboxspringboot.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.test.StepVerifier;
 
-@DataMongoTest
-public class UserRepositoryTest {
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static java.time.Month.JULY;
+import static org.assertj.core.api.Assertions.assertThat;
 
+public class UserRepositoryTest extends MongoIntegrationTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void insertUsers() {
+        var users = List.of(
+                new User("john", "doe", LocalDate.of(1989, JULY, 27)),
+                new User("jane", "doe", LocalDate.of(1989, JULY, 26)),
+                new User("jack", "smith", LocalDate.of(1989, JULY, 25)),
+                new User("rick", "sanchez", LocalDate.of(1989, JULY, 24)),
+                new User("morty", "smith", LocalDate.of(1989, JULY, 23))
+        );
+        var firstNames = users.stream().map(User::getFirstName).collect(Collectors.toList());
+        var savedUsers = userRepository.saveAll(users).cache();
+        StepVerifier.create(savedUsers.collectList())
+                .assertNext(s -> assertThat(s)
+                        .map(User::getFirstName)
+                        .containsExactlyElementsOf(firstNames))
+                .verifyComplete();
+    }
 }
