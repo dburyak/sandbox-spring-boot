b939856b76176315b10685a3d899274beb0754fa
Dmytro Buryak <dmytro.buryak@gmail.com>
2021-03-03 16:51:40 +0200

    Added mongo migrations

--- src/main/java/com/dburyak/sandbox/sandboxspringboot/domain/User.java
+++ src/main/java/com/dburyak/sandbox/sandboxspringboot/domain/User.java
@@ -10,13 +10,17 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
+import static com.dburyak.sandbox.sandboxspringboot.domain.User.COLLECTION_NAME;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 @RequiredArgsConstructor(onConstructor_ = {@PersistenceConstructor})
 @SuperBuilder(toBuilder = true)
-@Document(collection = "users")
+@Document(collection = COLLECTION_NAME)
 public class User extends PersistentVersionedEntity {
+    public static final String COLLECTION_NAME = "users";
+
     private final String firstName;
     private final String lastName;
     private final LocalDate birthDate;
---
+++ src/main/java/com/dburyak/sandbox/sandboxspringboot/migration/Changelog_001.java
@@ -0,0 +1,21 @@
+package com.dburyak.sandbox.sandboxspringboot.migration;
+
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import com.mongodb.client.MongoDatabase;
+import lombok.extern.log4j.Log4j2;
+import org.bson.Document;
+
+@ChangeLog(order = "001")
+@Log4j2
+public class Changelog_001 {
+
+    @ChangeSet(order = "001", id = "insert admin user", author = "dmytro.buryak@gmail.com")
+    public void insertAdminUser(MongoDatabase mongo) {
+        log.info("running mongock changeset: insert admin user");
+        mongo.getCollection("users").insertOne(Document.parse("{ " +
+                "firstName: \"admin\", " +
+                "lastName: \"admin\"" +
+                "}"));
+    }
+}
