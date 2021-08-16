022bc9223cf456e06f31f9da5c87730f95d48b37
Dmytro Buryak <dmytro.buryak@gmail.com>
2021-08-16 15:53:10 +0300

    Removed kafka as it is not used

--- src/test/java/com/dburyak/sandbox/sandboxspringboot/SandboxSpringBootApplicationTests.java
+++ src/test/java/com/dburyak/sandbox/sandboxspringboot/SandboxSpringBootApplicationTests.java
@@ -1,13 +1,11 @@
 package com.dburyak.sandbox.sandboxspringboot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SandboxSpringBootApplicationTests {
 
     @Test
-    void contextLoads() {
+    void test_placeholder() {
 
     }
 
